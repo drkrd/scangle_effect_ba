ae340e5767d753f3eb4acc743d6a7f25af9fa81c
Dayal Karun <karun.dayal@irstea.priv>
2020-09-08 11:02:49 +0200

    changed some variable names to make them homogenous across other codes

--- src/ba_models.R
+++ src/ba_models.R
@@ -1,17 +1,24 @@
-variation <- numeric(100)
-sd_lst <- numeric(100)
-r2_lst <- numeric(100)
-high_lst <- list()
-for(i in 1:1000)
+#variation <- numeric(100)
+library(lidR)
+library(dplyr)
+sd_lst <- list(1000)
+r2_lst <- list(1000)
+high_lst <- list(1000)
+for(loop in 1:1000)
 {
-  #print(i)
-  rand_dbs <- data.frame()
+
+  
+  mets_dbs_rand <- data.frame()
   min_count=0
+  
+  
   for(name in names(aois))
   {
     aoi <- lasflightline(aois[[name]])
     flist <- unique(aoi@data$flightlineID)
     meangle_fl <- data.frame()
+    
+    
     for(fl in flist)
     {
       aoi_subset <- lasfilter(aoi, flightlineID == fl)
@@ -19,6 +26,8 @@ for(i in 1:1000)
       meangle <- abs(mean(aoi_subset@data$ScanAngleRank))
       meangle_fl <- rbind(meangle_fl,c(meangle, fl, ar))
     }
+    
+    
     meangle_fl <- meangle_fl[meangle_fl[,3]>650,]
     x <- sample(1:length(meangle_fl[,1]),1)
     min_fl <- min(meangle_fl[,1])
@@ -31,27 +40,28 @@ for(i in 1:1000)
     aoi_varch <- func_varch(aoi_tmp@data$Z, aoi_tmp@data$ReturnNumber)
     aoi_pf <- func_pf(aoi_tmp@data$Z, aoi_tmp@data$ReturnNumber)
     aoi_cvlad <- func_cvlad(aoi_tmp@data$Z, aoi_tmp@data$ReturnNumber)
-    rand_dbs <- as.data.frame(rbind(rand_dbs, c(name,
-                                                meangle_fl[,1][x],
-                                                aoi_meanch,
-                                                aoi_varch,
-                                                aoi_pf,
-                                                aoi_cvlad)))
+    mets_dbs_rand <- as.data.frame(rbind(mets_dbs_rand, c(name,
+                                                          meangle_fl[,1][x],
+                                                          aoi_meanch,
+                                                          aoi_varch,
+                                                          aoi_pf,
+                                                          aoi_cvlad)))
   }
   
   
-  names(rand_dbs) <- c("id_placette", "mean_angle", "meanch", "varch", "pf", "cvlad")
-  rand_dbs$mean_angle <- as.numeric(rand_dbs$mean_angle)
-  rand_dbs$meanch <- log(as.numeric(rand_dbs$meanch))
-  rand_dbs$varch <- log(as.numeric(rand_dbs$varch))
-  rand_dbs$pf <- log(as.numeric(rand_dbs$pf))
-  rand_dbs$cvlad <- log(as.numeric(rand_dbs$cvlad))
+  names(mets_dbs_rand) <- c("id_placette", "mean_angle", "meanch", "varch", "pf", "cvlad")
+  mets_dbs_rand$id_placette <- as.factor(mets_dbs_rand$id_placette)
+  mets_dbs_rand$mean_angle <- as.numeric(mets_dbs_rand$mean_angle)
+  mets_dbs_rand$meanch <- as.numeric(mets_dbs_rand$meanch)
+  mets_dbs_rand$varch <- as.numeric(mets_dbs_rand$varch)
+  mets_dbs_rand$pf <- as.numeric(mets_dbs_rand$pf)
+  mets_dbs_rand$cvlad <- as.numeric(mets_dbs_rand$cvlad)
   
   
   
-  fdata1 <- right_join(fdata, rand_dbs, by="id_placette")
+  mets_dbs_rand <- right_join(fd_ba_smry, mets_dbs_rand, by="id_placette")
   
-  # fdata1 <- fdata1 %>% 
+  # fd_ba_smry1 <- fd_ba_smry1 %>% 
   #   mutate(Classes=if_else(mean_angle<5,1,
   #                          if_else(mean_angle<10,2,
   #                                  if_else(mean_angle<15,3,
@@ -62,19 +72,19 @@ for(i in 1:1000)
   #                                                                          if_else(mean_angle<40,8,9)))))))))
   
   
-  # vars <- length(unique(fdata1$Classes))
+  # vars <- length(unique(fd_ba_smry1$Classes))
   
   
-  sd_meangle <- sd(fdata1$mean_angle)
-  model <- lm(sum_ba_hec~meanch+varch+pf+cvlad, data = fdata1[-c(21,21),])
+  sd_meangle <- sd(mets_dbs_rand$mean_angle)
+  model_randang <- lm(data = mets_dbs_rand[-c(21,22),], 
+                      formula = log(sum_ba_hec)~log(meanch)+log(varch)+log(pf)+log(cvlad))
   
-  r2 <- summary(model)$adj.r.squared
-  high_lst[[toString(r2)]] <- fdata1$mean_angle
+  r2 <- summary(model_randang)$adj.r.squared
+  high_lst[[toString(r2)]] <- mets_dbs_rand$mean_angle
 
-  #print(sd_meangle)
-  variation[i] <- vars
-  sd_lst[i] <- sd_meangle
-  r2_lst[i] <- r2
-  
-  
+  #print(r2)
+  #variation[i] <- vars
+  sd_lst[[loop]] <- sd_meangle
+  r2_lst[[loop]] <- r2
+  print(loop)
 }
